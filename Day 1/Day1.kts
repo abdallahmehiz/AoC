fc3df0e973621cea3e3fee736e36733207a19639
Abdallah Mehiz <abdallahmehiz0@gmail.com>
2023-12-02 13:08:26 +0100

    second iteration of day1 part1

--- Day 1/Day1.kts
+++ Day 1/Day1.kts
@@ -1,20 +1,20 @@
 import java.io.File
 
 val numbers = mapOf(
-    "one" to '1',
-    "two" to '2',
-    "three" to '3',
-    "four" to '4',
-    "five" to '5',
-    "six" to '6',
-    "seven" to '7',
-    "eight" to '8',
-    "nine" to '9',
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
 )
 
 fun part1(input: List<String>): Int {
     return input.sumOf {
-        "${it.find { c -> numbers.containsValue(c) }}${it.findLast { c -> numbers.containsValue(c) }}"
+        "${it.find { c -> numbers.containsValue("$c") }}${it.findLast { c -> numbers.containsValue("$c") }}"
             .toInt()
     }
 }
@@ -27,6 +27,25 @@ fun part2(input: List<String>): Int {
     })
 }
 
+fun part2i2(input: List<String>) = input.sumOf {
+    var first = ""
+    var last = ""
+    it.findAnyOf(numbers.values)?.let { pair ->
+        first = pair.second
+        it.findAnyOf(numbers.keys)?.let { key ->
+            first = if (pair.first < key.first) pair.second else numbers[key.second]!!
+        }
+    }
+    it.findLastAnyOf(numbers.values)?.let { pair ->
+        last = pair.second
+        it.findLastAnyOf(numbers.keys)?.let { key ->
+            last = if (pair.first > key.first) pair.second else numbers[key.second]!!
+        }
+    }
+    "$first$last".toInt()
+}
+
 val input = File("input").readLines().map { it }.toMutableList()
 println("Part 1: ${part1(input)}")
 println("Part 2: ${part2(input)}")
+println("Part 2: ${part2i2(input)}")
--- Day 2/Day2.kts
+++ Day 2/Day2.kts
@@ -3,17 +3,17 @@ import kotlin.math.max
 
 fun part1(input: List<String>, possibleColors: Map<String, Int>): Int {
     return input.sumOf { line ->
-        val gameId = line.split(":")[0].replace("Game ","").toInt()
+        val gameId = line.split(":")[0].replace("Game ", "").toInt()
         var possible = true
         line.split(": ")[1].split("; ")
             .forEach { pull ->
-            val cubes = pull.split(", ")
-            cubes.forEach { cube ->
-                if(possibleColors[cube.split(" ")[1]]!! < cube.split(" ")[0].toInt())
-                    possible = false
+                val cubes = pull.split(", ")
+                cubes.forEach { cube ->
+                    if (possibleColors[cube.split(" ")[1]]!! < cube.split(" ")[0].toInt())
+                        possible = false
+                }
             }
-        }
-        if(possible) gameId else 0
+        if (possible) gameId else 0
     }
 }
 
@@ -26,10 +26,11 @@ fun part2(input: List<String>): Int {
         )
         line.split(": ")[1].split("; ")
             .forEach { pull ->
-            pull.split(", ").forEach {
-                minimum[it.split(" ")[1]] = max(minimum[it.split(" ")[1]]!!, it.split(" ")[0].toInt())
+                pull.split(", ").forEach {
+                    minimum[it.split(" ")[1]] =
+                        max(minimum[it.split(" ")[1]]!!, it.split(" ")[0].toInt())
+                }
             }
-        }
         minimum["red"]!! * minimum["green"]!! * minimum["blue"]!!
     }
 }
