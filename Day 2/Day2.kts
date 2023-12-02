b8572c6d3584435e09abb7841f640d4e5a3e0e10
Abdallah Mehiz <abdallahmehiz0@gmail.com>
2023-12-02 11:49:45 +0100

    even more optimizations

--- Day 2/Day2.kts
+++ Day 2/Day2.kts
@@ -5,10 +5,8 @@ fun part1(input: List<String>, possibleColors: Map<String, Int>): Int {
     return input.sumOf { line ->
         val gameId = line.split(":")[0].replace("Game ","").toInt()
         var possible = true
-        val pulls = line
-            .replace(regex = Regex("Game [0-9]*: "),"")
-            .split("; ")
-        pulls.forEach { pull ->
+        line.split(": ")[1].split("; ")
+            .forEach { pull ->
             val cubes = pull.split(", ")
             cubes.forEach { cube ->
                 if(possibleColors[cube.split(" ")[1]]!! < cube.split(" ")[0].toInt())
@@ -26,10 +24,8 @@ fun part2(input: List<String>): Int {
             "green" to 1,
             "blue" to 1,
         )
-        val pulls = line
-            .replace(regex = Regex("Game [0-9]*: "), "")
-            .split("; ")
-        pulls.forEach { pull ->
+        line.split(": ")[1].split("; ")
+            .forEach { pull ->
             pull.split(", ").forEach {
                 minimum[it.split(" ")[1]] = max(minimum[it.split(" ")[1]]!!, it.split(" ")[0].toInt())
             }
